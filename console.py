e1400e74aee4e3f168850d0fcb470e6c580ce6e5
esrAA312 <esraayahya312@gmail.com>
2024-02-10 08:27:40 -0800

    done

--- console.py
+++ console.py
@@ -24,9 +24,9 @@ class HBNBCommand(cmd.Cmd):
     hbnbcommand
     """
 
-    completekey = "tab"
+    completekey = 'tab'
     prompt = "(hbnb) "
-    CC = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
+    CC = ["BaseModel", "User", "Amenity", "Place", "Review", "State", "City"]
 
     def do_quit(self, arg):
         """
@@ -51,59 +51,57 @@ class HBNBCommand(cmd.Cmd):
         """
         CREATE.
         """
-        if not arg:
+        SP = shlex.split(arg)
+        N = SP[0]
+        if not (SP):
             print("** class name missing **")
+        elif N not in self.CC:
+            print("** class doesn't exist **")
         else:
-            if arg not in self.CC:
-                print("** class doesn't exist **")
-            else:
-                SP = shlex.split(arg)
-                yues = eval(sp[0])().id
-                print(yues)
-                storage.save()
+            yues = eval(N)().id
+            print(yues)
+            storage.save()
 
     def do_show(self, arg):
         """
         Show
         """
         SP = shlex.split(arg)
-        if len(SP) == 0:
+
+        if len(SP) == 0 or SP == "" or SP is None:
             print("** class name missing **")
+        elif SP[0] not in self.CC:
+            print("** class doesn't exist **")
+        elif len(SP) < 2 or len(SP) == 1:
+            print("** instance id missing **")
         else:
-            if SP[0] not in self.CC:
-                print("** class doesn't exist **")
-            elif len(SP) < 2:
-                print("** instance id missing **")
+            K = "{}.{}".format(SP[0], SP[1])
+            if K in storage.all():
+                print(storage.all()[K])
             else:
-                K = "{}.{}".format(SP[0], SP[1])
-                if K in storage.all():
-                    print(storage.all()[K])
-                else:
-                    print("** no instance found **")
-        return
+                print("** no instance found **")
 
     def do_destroy(self, arg):
         """destroy"""
         SP = shlex.split(arg)
         D = storage.all()
 
-        if len(SP) == 0:
+        if len(SP) == 0 or SP == "" or SP is None:
             print("** class name missing **")
             return
+        elif len(SP) == 1 or len(SP) < 2:
+            print("** instance id missing **")
+            return
+        elif SP[0] not in self.CC:
+            print("** class doesn't exist **")
+            return
         else:
-            if SP[0] not in self.CC:
-                print("** class doesn't exist **")
-                return
-            elif len(SP) < 2:
-                print("** instance id missing **")
-                return
+            key = SP[0] + "." + SP[1]
+            if key in D:
+                del D[key]
+                storage.save()
             else:
-                key = SP[0] + "." + SP[1]
-                if key in D:
-                    del D[key]
-                    storage.save()
-                else:
-                    print("** no instance found **")
+                print("** no instance found **")
 
     def do_all(self, arg):
         """
@@ -132,73 +130,77 @@ class HBNBCommand(cmd.Cmd):
         XX = shlex.split(arg)
 
         if len(XX) == 0:
-            print("** class name missing **")
+            print("* class name missing *")
         elif XX[0] not in self.CC:
-            print("** class doesn't exist **")
+            print("* class doesn't exist *")
         elif len(XX) < 2:
-            print("** instance id missing **")
+            print("* instance id missing *")
+        elif len(XX) < 3:
+            print("* attribute name missing *")
+        elif len(XX) < 4:
+            print("* value missing *")
         else:
             MYclassname, MYinstanceId, MYattributeName, MYname = XX[:4]
+
             K = f"{MYclassname}.{MYinstanceId}"
             DI = storage.all()
+
             ineU = DI.get(K)
+
             if ineU is None:
                 print("** no instance found **")
-            elif len(XX) < 3:
-                print("** attribute name missing **")
-            elif len(XX) < 4:
-                print("** value missing **")
-            else:
-                try:
-                    AT = type(getattr(ineU, MYattributeName))
-                    MYname = AT(MYname)
-                except AttributeError:
-                    pass
-                setattr(ineU, MYattributeName, MYname)
-                storage.save()
+                return
+
+            try:
+                AT = type(getattr(ineU, MYattributeName))
+                MYname = AT(MYname)
+            except AttributeError:
+                pass
+            setattr(ineU, MYattributeName, MYname)
+            storage.save()
 
     def default(self, arg):
         """Default behavior for cmd module when input is invalid"""
 
-        TT = {
+        argdict = {
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
             "update": self.do_update,
-            "count": self.do_count,
+            "count": self.do_count
         }
         match = re.search(r"\.", arg)
         if bool(match):
-            arg_list = arg.split(".")
-            clsS = arg_list[0]
+            arg_list = arg.split('.')
+            cls_nm = arg_list[0]
             start, end = match.span()
-            arGl = [arg[:start], arg[end:]]
-
-            match = re.search(r"\((.*?)\)", arGl[1])
+            argl = [arg[:start], arg[end:]]
+            
+            match = re.search(r"\((.*?)\)", argl[1])
             if bool(match):
                 start, end = match.span()
-                command = arg_list[1].split("(")
-                cmet = command[0]
-                e_arg = command[1].split(")")[0]
-                al = e_arg.split(",")
-                cotext = arGl[1][:start]
-                coarg = match.group()[1:-1]
-                command = [cotext, coarg]
+                command = arg_list[1].split('(')
+                cmd_met = command[0]
+                e_arg = command[1].split(')')[0]
+                al = e_arg.split(',')
+                command_text = argl[1][:start]
+                command_argument = match.group()[1:-1]
+                command = [command_text, command_argument]
 
-                if cmet in TT.keys():
-                    if cmet != "update":
-                        call = f"{arGl[0]} {e_arg}"
-                        return TT[cmet](call)
+                if cmd_met in argdict.keys():
+                    if cmd_met != "update":
+                        call = f"{argl[0]} {e_arg}"
+                        return argdict[cmd_met](call)
                     elif len(al) >= 2 and re.search(r"\{.*?\}", e_arg):
                         ob = al[0]
                         ana = al[1:]
                         ana[0] = ana[0].lstrip()
                         for i in range(1, len(ana)):
                             ana[i] = "," + ana[i]
-                        jostr = "".join(ana)
-                        result_dict = ast.literal_eval(jostr)
+                        joined_string = ''.join(ana)
+                        result_dict =                                                                         ast.literal_eval(joined_string)
                         for k, v in result_dict.items():
-                            TT[cmet]("{} {} {} {}".format(clsS, ob, k, v))
+                            argdict[cmd_met]("{} {} {} {}".format(cls_nm, ob, k, v))
                         return ""
 
                     elif len(al) == 3:
@@ -206,9 +208,7 @@ class HBNBCommand(cmd.Cmd):
                         ana = al[1:]
                         for i in range(0, len(ana)):
                             ana[i] = ana[i].lstrip()
-                            TT[cmet](
-                                "{} {} {} {}".format(clsS, ob, ana[0], ana[1])
-                            )
+                            argdict[cmd_met]("{} {} {} {}".format(cls_nm, ob, ana[0], ana[1]))
                         return ""
 
         print(f"*** Unknown syntax: {arg}")
@@ -219,15 +219,9 @@ class HBNBCommand(cmd.Cmd):
         """count()"""
 
         ar = shlex.split(arg)
-        if not ar:
-            print("** class name missing **")
-        elif ar[0] not in self.CC:
-            print("** class doesn't exist **")
-        else:
-
-            count = sum(1 for x in storage.all().values()
-                        if ar[0] == x.__class__.__name__)
-            print(count)
+        count = sum(1 for x in storage.all().values()
+                    if ar[0] == x.__class__.__name__)
+        print(count)
 
 
 if __name__ == "__main__":
