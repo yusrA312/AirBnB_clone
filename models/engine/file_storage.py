e79a5ab87c061a2e165471182f6a2dc25c5da3ef
esrAA312 <esraayahya312@gmail.com>
2024-02-10 03:58:37 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,6 +43,6 @@ class FileStorage:
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
+            obj_dict = {k: self.CC()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
             FileStorage.__objects = obj_dict
