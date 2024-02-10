9a48a11f4eea96b1fd18a9ae9675b2f3b5d8a19f
esrAA312 <esraayahya312@gmail.com>
2024-02-10 14:20:51 -0800

    done

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,16 +8,12 @@ from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
 
-
 class TestHBNB(unittest.TestCase):
     """Unittests for testing prompting of the HBNB command interpreter."""
 
     def test_smissing_cla(self):
         MSG = "** class name missing **"
-        CO = [
-            "show",
-            ".show()",
-        ]
+        CO = ["show", ".show()", ]
         for C in CO:
             with patch("sys.stdout", new=StringIO()) as OUT:
                 self.assertFalse(HBNBCommand().onecmd(C))
@@ -30,7 +26,6 @@ class TestHBNB(unittest.TestCase):
             with patch("sys.stdout", new=StringIO()) as OUT:
                 self.assertFalse(HBNBCommand().onecmd(C))
                 self.assertEqual(MSG, OUT.getvalue().strip())
-
     def test_desmissing_cla(self):
         MSG = "** class name missing **"
         CO = ["destroy", ".destroy()"]
@@ -38,7 +33,6 @@ class TestHBNB(unittest.TestCase):
             with patch("sys.stdout", new=StringIO()) as OUT:
                 self.assertFalse(HBNBCommand().onecmd(C))
                 self.assertEqual(MSG, OUT.getvalue().strip())
-
     def test_mis_1(self):
         X = "** class doesn't exist **"
         items = ["all", "count"]
@@ -55,22 +49,6 @@ class TestHBNB(unittest.TestCase):
                 self.assertFalse(HBNBCommand().onecmd(f"{item} kkk"))
                 self.assertEqual(X, FF.getvalue().strip())
 
-    def test_invalid_1(self):
-        items = ["create"]
-        for item in items:
-            X = f"*** Unknown syntax: kkk.{item}()"
-            with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
-                self.assertEqual(X, FF.getvalue().strip())
-
-    def test_mis_all(self):
-        X = "** class doesn't exist **"
-        items = ["count", "all"]
-        for item in items:
-            with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertFalse(HBNBCommand().onecmd(f"MyModel.{item}()"))
-                self.assertEqual(X, FF.getvalue().strip())
-
-
+    
 if __name__ == "__main__":
     unittest.main()
