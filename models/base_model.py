83e3ddb51adb11bed65569c7ec316e8492e15b9b
yusrA312 <yusra2yahya@gmail.com>
2024-02-11 05:07:06 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,9 @@ class BaseModel:
             for key, value in mykwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, ISO)
-
-                if key != "__class__":
-                    setattr(self, key, value)
+		else:
+                    if key != "__class__":
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
